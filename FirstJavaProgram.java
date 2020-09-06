bb8218fcd77d9a42c385de3b651e050df93ddd9d
Y.Nagadivya <54504787+YalamarthiNagadivya@users.noreply.github.com>
2020-09-06 18:04:51 +0530

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class FirstJavaProgram{
+        public static void main(String args[]){
+                // Write your code here
+                String name="Nagadivya";
+                System.out.print("Hi,Welcome  "+ name);
+                System.out.println("  to the world of");
+                System.out.print("JAVA");
+        }
+}
+
+
